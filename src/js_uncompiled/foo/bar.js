9118fa283c927b2e7e0368f6fe4e5406ef7b14ec
jeffw <jeffw>
2012-01-04 08:04:04 +0000

    Unit tests and some drag-n-drop stuff.

--- src/js_uncompiled/foo/bar.js
+++ src/js_uncompiled/foo/bar.js
@@ -6,13 +6,11 @@ goog.require('goog.dom');
  */
 closureexample.foo.Bar = function(parent) {
 	this.parent = parent;
-}
-;
+};
 
 closureexample.foo.Bar.prototype.showContent = function() {
 	var element = goog.dom.createDom('div', {
 		'style' : 'background:#FFFF00'
-	}, "This was created in the Bar class");
+	}, 'This was created in the Bar class');
 	goog.dom.appendChild(this.parent, element);
-}
-;
+};
--- src/js_uncompiled/init.js
+++ src/js_uncompiled/init.js
@@ -1,12 +1,12 @@
 goog.provide('closureexample.init');
-goog.require('goog.dom');
 goog.require('closureexample.foo.Bar');
+goog.require('goog.dom');
 
 closureexample.init = function(appTitle, parent) {
 	var header = {
 		'style' : 'background:#FF0000'
 	};
-	var content = "Application " + appTitle + " Starting";
+	var content = 'Application ' + appTitle + ' Starting';
 	var element = goog.dom.createDom('div', header, content);
 	goog.dom.appendChild(parent, element);
 
