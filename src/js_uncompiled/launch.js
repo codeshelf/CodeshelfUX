677dedb265da2c97a372198b28a5d088534a38ff
jeffw <jeffw>
2012-02-05 02:55:14 +0000

    WebSocket support starting!

--- src/js_uncompiled/launch.js
+++ src/js_uncompiled/launch.js
@@ -1,4 +1,6 @@
 goog.provide('codeshelf.launch');
+goog.require('codeshelf.templates');
+goog.require('soy');
 goog.require('goog.dom');
 goog.require('goog.dom.query');
 goog.require('goog.window');
@@ -8,6 +10,9 @@ goog.require('goog.events.EventType');
 goog.require('goog.ui.RoundedPanel');
 goog.require('goog.math.Size');
 goog.require('goog.dom.ViewportSizeMonitor');
+goog.require('goog.net.WebSocket');
+
+var websocket = new goog.net.WebSocket();
 
 function enterLaunchWindow() {
 //	var doc = goog.dom.getDocument();
@@ -21,14 +26,18 @@ function enterLaunchWindow() {
 
 	// Create DOM structure to represent the launch code.
 	//var launchCode = goog.dom.createDom('div', {'id':'launchCodeLabel'}, launchCodePanelContent);
-	var launchCodeInput = goog.dom.createDom('input', {'type':'text', 'id':'launchCodeInput', 'class':'dataEntry'}, null);
-	var launchCodeLabel = goog.dom.createDom('div', {'id':'launchCodeLabel', 'class':'dataEntry'}, 'Launch Code:', launchCodeInput);
-	var launchCodePanelContent = goog.dom.createDom('div', {'class':'goog-roundedpanel-content'}, launchCodeLabel);
-	launchCodeLabel.style.marginTop = '15px';
-	launchCodeLabel.style.marginLeft = '15px';
+//	var launchCodeInput = goog.dom.createDom('input', {'type':'text', 'id':'launchCodeInput', 'class':'dataEntry'}, null);
+//	var launchCodeLabel = goog.dom.createDom('div', {'id':'launchCodeLabel', 'class':'dataEntry'}, 'Launch Code:', launchCodeInput);
+//	var launchCodePanelContent = goog.dom.createDom('div', {'class':'goog-roundedpanel-content'}, launchCodeLabel);
+//	launchCodeLabel.style.marginTop = '15px';
+//	launchCodeLabel.style.marginLeft = '15px';
+//
+//	var launchCodePanel = goog.dom.createDom('div', {'id':'launchCodePanel'}, launchCodePanelContent);
+//	goog.dom.appendChild(goog.dom.getDocument().body, launchCodePanel);
 
-	var launchCodePanel = goog.dom.createDom('div', {'id':'launchCodePanel'}, launchCodePanelContent);
-	goog.dom.appendChild(goog.dom.getDocument().body, launchCodePanel);
+	//goog.dom.getDocument().body.innerHTML += codeshelf.templates.launchCodeDialog();
+	goog.dom.appendChild(goog.dom.getDocument().body, soy.renderAsElement(codeshelf.templates.launchCodeDialog));
+	var launchCodeInput = goog.dom.getElement('launchCodeInput');
 
 	// Set the dimensions of the panel and decorate roundedPanel.
 	var radius = 25;
@@ -37,13 +46,50 @@ function enterLaunchWindow() {
 	var backgroundColor = '#d0d0d0';//goog.style.getCascadedStyle(launchCodePanel, 'background-color');
 	var corners = 15;
 	var roundedLaunchCodePanel = goog.ui.RoundedPanel.create(radius, borderWidth, borderColor, backgroundColor, corners);
+	var launchCodePanel = goog.dom.getElement('launchCodePanel');
 	roundedLaunchCodePanel.decorate(launchCodePanel);
+
+	launchCodeInput.onchange = launchCodeCheck;
 }
 
+function initWebSocket() {
+	var handler = new goog.events.EventHandler();
+	handler.listen(websocket, goog.net.WebSocket.EventType.OPENED, onOpen);
+	handler.listen(websocket, goog.net.WebSocket.EventType.OPENED, onClose);
+	handler.listen(websocket, goog.net.WebSocket.EventType.MESSAGE, onMessage);
+
+	try {
+		websocket.open('ws://127.0.0.1:8080');
+	} catch (e) {
+		//
+	}
+}
+
+function onOpen() {
+	//alert('Open');
+}
+
+function onClose() {
+	//alert('Close');
+}
+
+function onMessage() {
+	//alert('Message');
+}
 
 function exitLaunchWindow() {
 
 }
 
+function launchCodeCheck() {
+	var launchCodeInput = goog.dom.getElement('launchCodeInput');
+	//alert('Launch code: ' + launchCodeInput.value);
+	websocket.send(launchCodeInput.value);
+}
 
-enterLaunchWindow();
+function initApplication() {
+	initWebSocket();
+	enterLaunchWindow();
+}
+
+initApplication();
