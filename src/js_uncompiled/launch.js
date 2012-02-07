fc13c2e7acb154c554e0d17225af85306620fa60
jeffw <jeffw>
2012-02-07 08:19:50 +0000

    WebSession behavior

--- src/js_uncompiled/launch.js
+++ src/js_uncompiled/launch.js
@@ -13,8 +13,6 @@ goog.require('goog.dom.ViewportSizeMonitor');
 goog.require('goog.net.WebSocket');
 goog.require('goog.json');
 
-var websocket = new goog.net.WebSocket();
-
 function enterLaunchWindow() {
 //	var doc = goog.dom.getDocument();
 //	doc.write('<img id="background_image" class="background_image" src="../src/images/STS-125.jpg" alt=""/>');
@@ -54,11 +52,36 @@ function enterLaunchWindow() {
 }
 
 function initWebSocket() {
+
+	/**
+	 * Strategy for reconnection that backs off linearly with a 1 second offset.
+	 * @param {number} attempt The number of reconnects since the last connection.
+	 * @return {number} The amount of time to the next reconnect, in milliseconds.
+	 */
+
+	codeshelfApp.attempt = 0;
+
+	function linearBackOff() {
+		return (codeshelfApp.attempt++ * 1000) + 1000;
+	}
+
+	var websocket = new goog.net.WebSocket(true, linearBackOff);
 	var handler = new goog.events.EventHandler();
 	handler.listen(websocket, goog.net.WebSocket.EventType.ERROR, onError);
 	handler.listen(websocket, goog.net.WebSocket.EventType.OPENED, onOpen);
 	handler.listen(websocket, goog.net.WebSocket.EventType.CLOSED, onClose);
 	handler.listen(websocket, goog.net.WebSocket.EventType.MESSAGE, onMessage);
+
+	try {
+		if (!websocket.isOpen()) {
+			websocket.open('ws://127.0.0.1:8080');
+			//while (!websocket.isOpen()) {
+			//}
+		}
+	} catch (e) {
+		//
+	}
+	return websocket;
 }
 
 function onError() {
@@ -71,11 +94,10 @@ function onOpen() {
 
 function onClose() {
 	alert('Close');
-	websocket.close();
 }
 
-function onMessage() {
-	//alert('Message');
+function onMessage(messageEvent) {
+	alert('Message:' + messageEvent.message);
 }
 
 function exitLaunchWindow() {
@@ -87,7 +109,7 @@ function launchCodeCheck() {
 	//alert('Launch code: ' + launchCodeInput.value);
 
 	// Put it into JSON format:
-	var result = {
+	var launchCommand = {
 		id:  0,
 		type:'LAUNCH_CODE',
 		data:{
@@ -96,23 +118,25 @@ function launchCodeCheck() {
 	}
 	// Use getUid to get a new UID for this object.
 	// (The call mutates the object, but we don't want to send that over the stream, so remove the mutation and just use the ID.)
-	result.id = goog.getUid(result);
-	goog.removeUid(result);
+	launchCommand.id = goog.getUid(launchCommand);
+	goog.removeUid(launchCommand);
 
+	// Attempt to send the command.
 	try {
-		if (!websocket.isOpen()) {
-			websocket.open('ws://127.0.0.1:8080');
-			//while (!websocket.isOpen()) {
-			//}
+		if (!codeshelfApp.websocket.isOpen()) {
+			Alert('WebSocket not open: try again later');
+		} else {
+			codeshelfApp.websocket.send(goog.json.serialize(launchCommand));
 		}
-		websocket.send(goog.json.serialize(result));
 	} catch (e) {
-		//
+
 	}
 }
 
+var codeshelfApp = {};
+
 function initApplication() {
-	initWebSocket();
+	codeshelfApp.websocket = initWebSocket();
 	enterLaunchWindow();
 }
 
