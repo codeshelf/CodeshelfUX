f27f2284f4e6f0a89ab6539feb358dbb89b2b037
jeffw <jeffw>
2012-10-31 16:55:07 +0000

    Some more post hierarchical object refactor cleanup.

--- src/js_uncompiled/domainObjects.js
+++ src/js_uncompiled/domainObjects.js
@@ -1,23 +1,81 @@
 /*******************************************************************************
  * CodeShelfUX Copyright (c) 2005-2012, Jeffrey B. Williams, All rights reserved
- * $Id: domainObjects.js,v 1.24 2012/10/30 15:21:33 jeffw Exp $
+ * $Id: domainObjects.js,v 1.25 2012/10/31 16:55:07 jeffw Exp $
  ******************************************************************************/
 
 goog.provide('domainobjects');
 
 domainobjects = {
 
-	Organization: {
-		className:  'Organization',
+	Aisle: {
+		className:  'Aisle',
 		properties: {
 			domainId: {
 				id:    'domainId',
 				title: 'ID',
 				width: 25
+			}
+		}
+	},
+
+	Bay: {
+		className:  'Bay',
+		properties: {
+			domainId: {
+				id:    'domainId',
+				title: 'ID',
+				width: 25
+			}
+		}
+	},
+
+	DropboxService: {
+		className:  'DropboxService',
+		properties: {
+			domainId:           {
+				id:    'domainId',
+				title: 'Parent ID',
+				width: 25
 			},
-			desc:     {
-				id:    'description',
-				title: 'Organization Name',
+			parentFullDomainId: {
+				id:    'parentFullDomainId',
+				title: 'Parent ID',
+				width: 50
+			},
+			provider:           {
+				id:    'providerEnum',
+				title: 'Provider',
+				width: 40
+			},
+			serviceState:       {
+				id:    'serviceStateEnum',
+				title: 'Service State',
+				width: 40
+			}
+		}
+	},
+
+	EdiDocumentLocator: {
+		className:  'EdiDocumentLocator',
+		properties: {
+			domainId:           {
+				id:    'fullDomainId',
+				title: 'Parent ID',
+				width: 25
+			},
+			parentFullDomainId: {
+				id:    'parentFullDomainId',
+				title: 'ID',
+				width: 50
+			},
+			documentState:      {
+				id:    'documentStateEnum',
+				title: 'Doc State',
+				width: 40
+			},
+			documentName:       {
+				id:    'documentName',
+				title: 'Document Name',
 				width: 100
 			}
 		}
@@ -26,7 +84,7 @@ domainobjects = {
 	Facility: {
 		className:  'Facility',
 		properties: {
-			domainId: {
+			domainId:           {
 				id:    'fullDomainId',
 				title: 'ID',
 				width: 25
@@ -36,7 +94,7 @@ domainobjects = {
 				title: 'Parent ID',
 				width: 50
 			},
-			desc:     {
+			desc:               {
 				id:    'description',
 				title: 'Facility Name',
 				width: 50
@@ -64,13 +122,86 @@ domainobjects = {
 		}
 	},
 
-	location: {
+	LocationABC: {
 		className:  'Location',
 		properties: {
 			domainId: {
 				id:    'domainId',
 				title: 'ID',
 				width: 25
+			}
+		}
+	},
+
+	OrderDetail: {
+		className:  'OrderDetail',
+		properties: {
+			domainId:    {
+				id:    'fullDomainId',
+				title: 'ID',
+				width: 25
+			},
+			detailId:    {
+				id:    'domainId',
+				title: 'Detail ID',
+				width: 15
+			},
+			status:      {
+				id:    'statusEnum',
+				title: 'Status',
+				width: 25
+			},
+			sku:         {
+				id:    'itemMasterId',
+				title: 'SKU',
+				width: 20
+			},
+			description: {
+				id:    'description',
+				title: 'Description',
+				width: 50
+			},
+			quantity:    {
+				id:    'quantity',
+				title: 'Qty',
+				width: 10
+			},
+			uom:         {
+				id:    'uomMasterId',
+				title: 'UOM',
+				width: 10
+			}
+		}
+	},
+
+	OrderHeader: {
+		className:  'OrderHeader',
+		properties: {
+			domainId: {
+				id:    'fullDomainId',
+				title: 'ID',
+				width: 25
+			},
+			orderId:  {
+				id:    'orderId',
+				title: 'Order ID',
+				width: 15
+			}
+		}
+	},
+
+	Organization: {
+		className:  'Organization',
+		properties: {
+			domainId: {
+				id:    'domainId',
+				title: 'ID',
+				width: 25
+			},
+			desc:     {
+				id:    'description',
+				title: 'Organization Name',
+				width: 100
 			}
 		}
 	},
@@ -86,135 +217,5 @@ domainobjects = {
 		}
 	},
 
-	Aisle: {
-		className:  'Aisle',
-		properties: {
-			domainId: {
-				id:    'domainId',
-				title: 'ID',
-				width: 25
-			}
-		}
-	},
-
-	Bay: {
-		className:  'Bay',
-		properties: {
-			domainId: {
-				id:    'domainId',
-				title: 'ID',
-				width: 25
-			}
-		}
-	},
-
-	OrderHeader: {
-		className:  'OrderHeader',
-		properties: {
-			domainId: {
-				id:    'fullDomainId',
-				title: 'ID',
-				width: 25
-			},
-			orderId: {
-				id:    'orderId',
-				title: 'Order ID',
-				width: 15
-			}
-		}
-	},
-
-	OrderDetail: {
-		className:  'OrderDetail',
-		properties: {
-			domainId: {
-				id:    'fullDomainId',
-				title: 'ID',
-				width: 25
-			},
-			detailId: {
-				id:    'domainId',
-				title: 'Detail ID',
-				width: 15
-			},
-			status: {
-				id:    'statusEnum',
-				title: 'Status',
-				width: 25
-			},
-			sku: {
-				id:    'itemMasterId',
-				title: 'SKU',
-				width: 20
-			},
-			description: {
-				id:    'description',
-				title: 'Description',
-				width: 50
-			},
-			quantity: {
-				id:    'quantity',
-				title: 'Qty',
-				width: 10
-			},
-			uom: {
-				id:    'uomMasterId',
-				title: 'UOM',
-				width: 10
-			}
-		}
-	},
-
-	DropboxService: {
-		className:  'DropboxService',
-		properties: {
-			domainId: {
-				id:    'domainId',
-				title: 'Parent ID',
-				width: 25
-			},
-			parentFullDomainId: {
-				id:    'parentFullDomainId',
-				title: 'Parent ID',
-				width: 50
-			},
-			provider:     {
-				id:    'providerEnum',
-				title: 'Provider',
-				width: 40
-			},
-			serviceState:     {
-				id:    'serviceStateEnum',
-				title: 'Service State',
-				width: 40
-			}
-		}
-	},
-
-	EdiDocumentLocator: {
-		className:  'EdiDocumentLocator',
-		properties: {
-			domainId: {
-				id:    'domainId',
-				title: 'Parent ID',
-				width: 25
-			},
-			parentFullDomainId: {
-				id:    'parentFullDomainId',
-				title: 'ID',
-				width: 50
-			},
-			documentState:     {
-				id:    'documentStateEnum',
-				title: 'Doc State',
-				width: 40
-			},
-			documentName:     {
-				id:    'documentName',
-				title: 'Document Name',
-				width: 100
-			}
-		}
-	}
 
 };
