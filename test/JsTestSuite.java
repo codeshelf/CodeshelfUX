8be5539fad6a13fc20ac1f03f86cac510e3164da
jeffw <jeffw>
2012-11-25 08:44:15 +0000

    Add support for JsTestDriver Add support for JsTestRunner

---
+++ test/JsTestSuite.java
@@ -0,0 +1,13 @@
+import org.codehaus.jstestrunner.junit.JSTestSuiteRunner;
+import org.codehaus.jstestrunner.junit.JSTestSuiteRunner.ResourceBase;
+import org.junit.runner.RunWith;
+
+/**
+ * Run all JS tests associated with this project.
+ */
+@RunWith(JSTestSuiteRunner.class)
+@ResourceBase(value="test")
+public class JsTestSuite {
+
+
+}
