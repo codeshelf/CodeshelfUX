6b8ab989e62dedac6cf35cab3df3f4377c8999ee
Paul Monteiro <paul.monteiro@codeshelf.com>
2014-01-23 22:28:11 -0800

    Handle error messages returned from the server object method request, and display in the angular dialog; The dialog now remains until the send command is succesful

--- src/js_uncompiled/codeshelf.controllers.js
+++ src/js_uncompiled/codeshelf.controllers.js
@@ -1,131 +1,163 @@
 /*******************************************************************************
- *  CodeShelfUX
- *  Copyright (c) 2005-2013, Jeffrey B. Williams, All rights reserved
+ *	CodeShelfUX
+ *	Copyright (c) 2005-2013, Jeffrey B. Williams, All rights reserved
  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ *  $Id: application.js,v 1.24 2012/12/07 08:58:02 jeffw Exp $
  *******************************************************************************/
 
 'use strict';
+goog.require('codeshelf.websession');
 
 var codeshelfApp = angular.module('codeshelfApp', [
-        'ui.bootstrap'
-    ]).factory('$websession', function() {
-        return application.getWebsession();
-    })
-    /*.config(['$routeProvider',
-        function($routeProvider) {
-            $routeProvider.
-                when('/login', {
-                    templateUrl: 'partials/login.html',
-                    controller: 'LoginCtrl'
-                }).
-                when('/main', {
-                    templateUrl: 'partials/main.html',
-                    controller: 'MainlCtrl'
-                }).
-                otherwise({
-                    redirectTo: '/login'
-                });
-        }])
-        */;
+		'ui.bootstrap'
+	]).factory('$websession', function() {
+		return application.getWebsession();
+	})
+	/*.config(['$routeProvider',
+		function($routeProvider) {
+			$routeProvider.
+				when('/login', {
+					templateUrl: 'partials/login.html',
+					controller: 'LoginCtrl'
+				}).
+				when('/main', {
+					templateUrl: 'partials/main.html',
+					controller: 'MainlCtrl'
+				}).
+				otherwise({
+					redirectTo: '/login'
+				});
+		}])
+		*/;
 
-var WorkAreaCtrl = codeshelfApp.controller('WorkAreaCtrl', ['$scope', '$modal', '$websession', '$log', function($scope, $modal, $websession, $log) {
-    var consts = {};
-    Object.defineProperty(consts, 'feetInMeters', {value: 0.3048,
-        writable:                                         false,
-        enumerable:                                       true,
-        configurable:                                     true});
+var WorkAreaCtrl = codeshelfApp.controller('WorkAreaCtrl', ['$scope', '$modal', '$log', function($scope, $modal, $log) {
 
-    $scope.open = function (facilityContext, pixelsPerMeter, startDragPoint, currentRect, commandCallback) {
-        //TODO facility might be able to come from a parent controller
-        $scope.facilityContext = facilityContext;
-        $scope.pixelsPerMeter = pixelsPerMeter;
-        $scope.startDragPoint = startDragPoint;
-        $scope.currentRect_ = currentRect;
-        $scope.commandCallback = commandCallback;
-        $scope.aisleForm = {};
-        $scope.websession = $websession;
-        var modalInstance = $modal.open({
-            templateUrl: 'createAisleModalContent.html',
-            controller: ModalInstanceCtrl,
-            resolve: {
-                aisleForm: function () {
-                    return $scope.aisleForm;
-                }
-            }
-        });
+	$scope.open = function (facilityContext, aisleShape) {
+		//TODO facility might be able to come from a parent controller
+		var modalInstance = $modal.open({
+			templateUrl: 'createAisleModalContent.html',
+			controller: WorkAreaModalCtrl,
+			resolve: {
+				facilityContext: function() {
+					return facilityContext;
+				},
+				aisleShape: function () {
+					return aisleShape;
+				}
+			}
+		});
 
-        modalInstance.result.then(function (aisleForm) {
-            $scope.onSave(aisleForm);
-        }, function () {
-            $log.info('Modal dismissed at: ' + new Date());
-        });
-    };
+		modalInstance.result.then(function (aisleData) {
+			$log.info('Modal saved at: ' + new Date());
+		}, function () {
+			$log.info('Modal dismissed at: ' + new Date());
+		});
 
-    $scope.pixelsToMeter = function (pixels) {
-        return (pixels / this.pixelsPerMeter);
-    }
-
-    $scope.onSave = function (aisleForm) {
-        var aisle  = $scope.convertData(aisleForm);
-        $log.info('aisle obj: ' + angular.toJson(aisle));
-        $scope.sendCreateAisleCommand(aisle);
-    };
-
-    $scope.convertData = function (aisleForm) {
-        var aisle = {};
-        aisle.xOriginMeters = $scope.pixelsToMeter(this.startDragPoint.x);
-        aisle.yOriginMeters = $scope.pixelsToMeter(this.startDragPoint.y);
-
-        aisle.aisleId = aisleForm['aisleId'];
-        aisle.bayHeight = aisleForm['bayHeight'] * consts['feetInMeters'];
-        aisle.bayWidth = aisleForm['bayWidth'] * consts['feetInMeters'];
-        aisle.bayDepth = aisleForm['bayDepth'] * consts['feetInMeters'];
-        aisle.baysHigh = aisleForm['baysHigh'];
-        aisle.baysLong = aisleForm['baysLong'];
-        aisle.opensLowSide = aisleForm['opensLowSide'];
-
-
-        aisle.runInXDim = true;
-        if ($scope.currentRect_.width < $scope.currentRect_.height) {
-            aisle.runInXDim = false;
-        }
-        return aisle;
-    };
-
-    $scope.sendCreateAisleCommand = function(aisle) {
-        // Call Facility.createAisle();
-        var data = {
-            'className':    $scope.facilityContext.className,
-            'persistentId': $scope.facilityContext.facility['persistentId'],
-            'methodName':   'createAisle',
-            'methodArgs':   [
-                { 'name': 'inAisleId', 'value': aisle.aisleId, 'classType': 'java.lang.String'},
-                { 'name': 'inPosXMeters', 'value': aisle.xOriginMeters, 'classType': 'java.lang.Double'},
-                { 'name': 'inPosYMeters', 'value': aisle.yOriginMeters, 'classType': 'java.lang.Double'},
-                { 'name': 'inProtoBayXDimMeters', 'value': aisle.bayWidth, 'classType': 'java.lang.Double'},
-                { 'name': 'inProtoBayYDimMeters', 'value': aisle.bayDepth, 'classType': 'java.lang.Double'},
-                { 'name': 'inProtoBayZDimMeters', 'value': aisle.bayHeight, 'classType': 'java.lang.Double'},
-                { 'name': 'inProtoBaysHigh', 'value': aisle.baysHigh, 'classType': 'java.lang.Integer'},
-                { 'name': 'inProtoBaysLong', 'value': aisle.baysLong, 'classType': 'java.lang.Integer'},
-                { 'name': 'inRunInXDir', 'value': aisle.runInXDim, 'classType': 'java.lang.Boolean'},
-                { 'name': 'inOpensLowSide', 'value': aisle.opensLowSide, 'classType': 'java.lang.Boolean'}
-            ]
-        };
-
-        var createAisleCmd = $scope.websession.createCommand(kWebSessionCommandType.OBJECT_METHOD_REQ, data);
-        $scope.websession.sendCommand(createAisleCmd, $scope.commandCallback, true);
-    };
+	};
 
 }]);
 
-var ModalInstanceCtrl = function ($scope, $modalInstance, aisleForm) {
-    $scope.aisleForm = {};
-    $scope.ok = function () {
-        $modalInstance.close($scope.aisleForm);
-    };
 
-    $scope.cancel = function () {
-        $modalInstance.dismiss('cancel');
-    };
+
+
+var WorkAreaModalCtrl = function ($scope, $log, $modalInstance, $websession, facilityContext, aisleShape) {
+
+	var consts = {};
+	Object.defineProperty(consts, 'feetInMeters', {value: 0.3048,
+		writable:      false,
+		enumerable:    true,
+		configurable:  true});
+
+	$scope.aisleForm = {};
+	$scope.aisleForm = {
+		messages: []
+	};
+
+	$scope.websession = $websession;
+	$scope.facilityContext = facilityContext;
+	$scope.aisleShape = aisleShape;
+
+	$scope.ok = function () {
+		var aisle  = $scope.convertData($scope.aisleForm);
+		$log.info('aisle obj: ' + angular.toJson(aisle));
+		$scope.sendCreateAisleCommand(aisle,
+			function onSuccess(lastAisleData) {
+				$modalInstance.close(lastAisleData);  //communicates to the promise
+			},
+			function onError(errorResult) {
+				var responseMessages = errorResult['messages'];
+				$scope.aisleForm.messages = responseMessages;
+			});
+
+		//don't close instance until successful
+		//
+	};
+
+	$scope.cancel = function () {
+		$modalInstance.dismiss('cancel');
+	};
+
+	$scope.convertData = function (aisleForm) {
+		var aisle = {};
+		//TODO this should be a proper OO method on an aisle shape since the point and the pixelsPerMeter come from outside in the shape
+		aisle.xOriginMeters = $scope.pixelsToMeter($scope.aisleShape.dragStartPoint.x);
+		aisle.yOriginMeters = $scope.pixelsToMeter($scope.aisleShape.dragStartPoint.y);
+
+		aisle.aisleId = aisleForm['aisleId'];
+		aisle.bayHeight = aisleForm['bayHeight'] * consts['feetInMeters'];
+		aisle.bayWidth = aisleForm['bayWidth'] * consts['feetInMeters'];
+		aisle.bayDepth = aisleForm['bayDepth'] * consts['feetInMeters'];
+		aisle.baysHigh = aisleForm['baysHigh'];
+		aisle.baysLong = aisleForm['baysLong'];
+		aisle.opensLowSide = aisleForm['opensLowSide'] == "true";
+
+
+		aisle.runInXDim = true;
+		if ($scope.aisleShape.rectangle.width < $scope.aisleShape.rectangle.height) {
+			aisle.runInXDim = false;
+		}
+		return aisle;
+	};
+
+	$scope.pixelsToMeter = function (pixels) {
+		return (pixels / $scope.aisleShape.pixelsPerMeter);
+	};
+
+	$scope.sendCreateAisleCommand = function(aisle, onSuccess, onError) {
+		// Call Facility.createAisle();
+		var data = {
+			'className':	$scope.facilityContext.className,
+			'persistentId': $scope.facilityContext.facility['persistentId'],
+			'methodName':	'createAisle',
+			'methodArgs':	[
+				{ 'name': 'inAisleId', 'value': aisle.aisleId, 'classType': 'java.lang.String'},
+				{ 'name': 'inPosXMeters', 'value': aisle.xOriginMeters, 'classType': 'java.lang.Double'},
+				{ 'name': 'inPosYMeters', 'value': aisle.yOriginMeters, 'classType': 'java.lang.Double'},
+				{ 'name': 'inProtoBayXDimMeters', 'value': aisle.bayWidth, 'classType': 'java.lang.Double'},
+				{ 'name': 'inProtoBayYDimMeters', 'value': aisle.bayDepth, 'classType': 'java.lang.Double'},
+				{ 'name': 'inProtoBayZDimMeters', 'value': aisle.bayHeight, 'classType': 'java.lang.Double'},
+				{ 'name': 'inProtoBaysHigh', 'value': aisle.baysHigh, 'classType': 'java.lang.Integer'},
+				{ 'name': 'inProtoBaysLong', 'value': aisle.baysLong, 'classType': 'java.lang.Integer'},
+				{ 'name': 'inRunInXDir', 'value': aisle.runInXDim, 'classType': 'java.lang.Boolean'},
+				{ 'name': 'inOpensLowSide', 'value': aisle.opensLowSide, 'classType': 'java.lang.Boolean'}
+			]
+		};
+
+		var createAisleCmd = $scope.websession.createCommand(kWebSessionCommandType.OBJECT_METHOD_REQ, data);
+		var callback = {
+			exec: function(response) {
+				$scope.$apply(function() {
+					if (response['data']['status'] == "ERROR") {
+						var errorResult = response['data']['results'];
+						onError(errorResult);
+					}
+					else {
+						var dataResult = response['data']['results'];
+						onSuccess(dataResult);
+					}
+				});
+			}
+		};
+		$scope.websession.sendCommand(createAisleCmd, callback, true);
+   };
+
 };
-
--- test/unit/controllers.spec.js
+++ test/unit/controllers.spec.js
@@ -3,153 +3,186 @@
 /* jasmine specs for controllers go here */
 describe('Codeshelf controllers', function() {
 
-  /* Run before each describe and it function */
-  beforeEach(function(){
-      //Include all modules required for controller under test
-      module('codeshelfApp');
-  });
+	/* Run before each describe and it function */
+	beforeEach(function(){
+		//Include all modules required for controller under test
+		module('codeshelfApp');
+	});
 
-  describe('WorkAreaCtrl', function(){
-    var $scope, $modal, $websession, $q, createController;
+	describe('WorkAreaModalCtrl', function(){
+		var $scope, $modalInstance, $websession, $q, createController;
 
-    beforeEach(function() {
-        // Create spys for mock objects and list the methods you are going to fake
-        $websession = jasmine.createSpyObj('$websession', ['createCommand', 'sendCommand']);
-        $modal = jasmine.createSpyObj('$modal', ['open']);
-        var deferred;
-
-        // Create fake stubs for the methods
-        $modal.open.andCallFake(function () { deferred = $q.defer(); return {result: deferred.promise}; });
-
-        // Set up a module that provides your spy objects to the injector
-        module(function($provide) {
-            $provide.value('$websession', $websession);
-            $provide.value('$modal', $modal);
-        });
-
-        //Inject will look up your services and mocks and provide them to the function from which you can create
-        // closures to create controllers properly
-        // the underscore variables are a way to keep from colliding with vars in your environment, yet still look up
-        //   service by name
-        inject(function(_$q_, $log, $rootScope, $controller, _$modal_, _$websession_) {
-            $q = _$q_;
-            $scope = $rootScope.$new();
-
-            createController = function() {
-                return $controller('WorkAreaCtrl', {
-                    '$scope': $scope,
-                    '$modal': _$modal_,
-                    '$websession': _$websession_,
-                    '$log': $log
-                });
-            };
-        })
-    });
-
-    it('should have a method to open a dialog', function() {
-      var controller = createController();
-      $scope.open({}, {}, {});
-    });
-
-    describe('when the dialog is saved ', function() {
-        var fakeCommand;
-        var facilityContext, commandCallback;
-        var rectangle;
-
-        beforeEach(function(){
-          fakeCommand = {"same":"same"};
-
-          $websession.createCommand.andCallFake(function() { return fakeCommand});
-          var controller = createController();
-          facilityContext = {
-              facility: {persistentId: 99},
-              className:  "FACILITYCLASS"
-          };
-          var dragPoint = {
-              x: 48,
-              y: 24
-          };
-          rectangle = {
-              width: 10,
-              height: 12
-          };
-
-          commandCallback = function() {};
-          var aisleForm = {
-              'aisleId': '1A',
-	      'bayHeight': 3,
-              'bayWidth': 2,
-              'bayDepth' : 1,
-	      'baysHigh' : 4,
-	      'baysLong' : 5,
-	      'opensLowSide' : 'true'
-
-          };
-          $scope.open(facilityContext, 6, dragPoint, rectangle, commandCallback);
-          $scope.onSave(aisleForm);
-
-      });
-
-      describe('the command data', function() {
-         var data;
-         beforeEach(function() {
-             data = $websession.createCommand.mostRecentCall.args[1];
-         });
-
-          it ("should have classname from facility context", function() {
-              expect(data.className).toEqual(facilityContext.className);
-          });
-
-          it("should convert xOriginMeters from pixels to meters", function(){
-              expect(data.methodArgs[1].value).toEqual(48 / 6);
-          });
-
-          it("should convert yOriginMeters from pixels to meters", function(){
-              expect(data.methodArgs[2].value).toEqual(24 / 6);
-          });
-          
-          
-          it("should convert bayWidth to meters", function(){
-              expect(data.methodArgs[3].value).toEqual(2 * 0.3048);
-          });
-
-          it("should convert bayDepth to meters", function() {
-              expect(data.methodArgs[4].value).toEqual(1 * 0.3048);
-          });
-
-          it("should convert bayHeight to meters", function() {
-              expect(data.methodArgs[5].value).toEqual(3 * 0.3048);
-          });
-
-          it("should transfer  baysHigh", function() {
-              expect(data.methodArgs[6].value).toEqual(4);
-          });
-
-          it("should transfer baysLong", function() {
-              expect(data.methodArgs[7].value).toEqual(5);
-          });
-
-          it("should transfer opensLowSide", function() {
-              expect(data.methodArgs[9].value).toEqual('true');
-          });
-
-          it("should have an aisle id", function(){
-              expect(data.methodArgs[0].name).toEqual('inAisleId');
-              expect(data.methodArgs[0].value).toEqual('1A');
-          });
-
-          it("should set runInXDim to false if rectangle is higher than width", function() {
-              expect(rectangle.height).toBeGreaterThan(rectangle.width);
-              expect(data.methodArgs[8].value).toEqual(false);
-          });
+		beforeEach(function() {
+			// Create spys for mock objects and list the methods you are going to fake
+			$websession = jasmine.createSpyObj('$websession', ['createCommand', 'sendCommand']);
+			$modalInstance = jasmine.createSpyObj('$modalInstance', ['close','dismiss']);
 
 
-      });
+			// Create fake stubs for the methods
+			// Set up a module that provides your spy objects to the injector
+			module(function($provide) {
+				$provide.value('$websession', $websession);
+				$provide.value('$modalInstance', $modalInstance);
+			});
 
-      it("should call sendCommand with command and callback", function() {
-          expect($websession.sendCommand).toHaveBeenCalledWith(fakeCommand, commandCallback, true);
-      });
+			//Inject will look up your services and mocks and provide them to the function
+			//  from which you can create closures to create controllers properly
+			// the underscore variables are a way to keep from colliding with vars
+			// in your environment, yet still look up service by name
+			inject(function(_$q_, $log, $rootScope, _$modalInstance_, _$websession_) {
+				$q = _$q_;
+				$scope = $rootScope.$new();
 
-    });
-  });
+				createController = function(facilityContext, aisleShape) {
+					return WorkAreaModalCtrl($scope,
+						$log,
+						$modalInstance,
+						$websession,
+						facilityContext,
+						aisleShape);
+				};
+			});
+		});
+
+		describe('when the dialog is saved ', function() {
+			var fakeCommand;
+			var facilityContext;
+			var aisleShape;
+
+			beforeEach(function(){
+				fakeCommand = {"same":"same"};
+
+				$websession.createCommand.andCallFake(function() { return fakeCommand});
+
+				facilityContext = {
+					facility: {persistentId: 99},
+					className:  "FACILITYCLASS"
+				};
+				aisleShape = {
+					pixelsPerMeter: 6,
+					rectangle: {
+						width: 10,
+						height: 12
+					},
+					dragStartPoint: {
+						x: 48,
+						y: 24
+					}
+				};
+				var aisleForm = {
+					'aisleId': '1A',
+					'bayHeight': 3,
+					'bayWidth': 2,
+					'bayDepth' : 1,
+					'baysHigh' : 4,
+					'baysLong' : 5,
+					'opensLowSide' : 'true'
+				};
+				var controller = createController(facilityContext, aisleShape);
+				$scope.aisleForm = aisleForm;
+			});
+
+			describe('and the websession command fails', function() {
+				var messagesFromCommand;
+				beforeEach(function(){
+					messagesFromCommand = ['message1', 'message2'];
+					$websession.sendCommand.andCallFake(function(inCommand, inCallback, inRemainActive) {
+						var response = {
+							data: {
+								status: "ERROR",
+								results: {
+									messages: messagesFromCommand
+								}
+							}
+						};
+						inCallback.exec(response);
+					});
+					$scope.ok();
+				});
+
+				it('should transfer the messages to the scope', function() {
+					expect($scope.aisleForm.messages).toEqual(messagesFromCommand);
+				});
+
+				it('should not close the dialog', function(){
+					expect($modalInstance.close.calls.length).toEqual(0);
+				});
+			});
+
+			describe('the websession command succeeds', function() {
+				beforeEach(function(){
+					$websession.sendCommand.andCallFake(function(inCommand, inCallback, inRemainActive) {
+						var response = {
+							data: {
+								status: "OK",
+								results: {
+									whatever: 'notsure'
+
+								}
+							}
+						};
+						inCallback.exec(response);
+					});
+					$scope.ok();
+				});
+				it('should close the dialog', function() {
+					expect($modalInstance.close).toHaveBeenCalled();
+				});
+			});
+
+			describe('the command data', function() {
+				var data;
+				beforeEach(function() {
+					$scope.ok();
+					data = $websession.createCommand.mostRecentCall.args[1];
+				});
+
+				it ("should have classname from facility context", function() {
+					expect(data.className).toEqual(facilityContext.className);
+				});
+
+				it("should convert xOriginMeters from pixels to meters", function(){
+					expect(data.methodArgs[1].value).toEqual(48 / 6);
+				});
+
+				it("should convert yOriginMeters from pixels to meters", function(){
+					expect(data.methodArgs[2].value).toEqual(24 / 6);
+				});
+
+				it("should convert bayWidth to meters", function(){
+					expect(data.methodArgs[3].value).toEqual(2 * 0.3048);
+				});
+
+				it("should convert bayDepth to meters", function() {
+					expect(data.methodArgs[4].value).toEqual(1 * 0.3048);
+				});
+
+				it("should convert bayHeight to meters", function() {
+					expect(data.methodArgs[5].value).toEqual(3 * 0.3048);
+				});
+
+				it("should transfer  baysHigh", function() {
+					expect(data.methodArgs[6].value).toEqual(4);
+				});
+
+				it("should transfer baysLong", function() {
+					expect(data.methodArgs[7].value).toEqual(5);
+				});
+
+				it("should transfer opensLowSide", function() {
+					expect(data.methodArgs[9].value).toEqual(true);
+				});
+
+				it("should have an aisle id", function(){
+					expect(data.methodArgs[0].name).toEqual('inAisleId');
+					expect(data.methodArgs[0].value).toEqual('1A');
+				});
+
+				it("should set runInXDim to false if rectangle is higher than width", function() {
+					expect(aisleShape.rectangle.height).toBeGreaterThan(aisleShape.rectangle.width);
+					expect(data.methodArgs[8].value).toEqual(false);
+				});
+			});
+		});
+	});
 });
