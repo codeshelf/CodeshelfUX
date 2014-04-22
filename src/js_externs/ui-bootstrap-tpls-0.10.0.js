5018bfa216038a939f292a1a20c71d1392ea6af8
Paul Monteiro <paul.monteiro@codeshelf.com>
2014-04-22 08:01:35 -0700

    Corrected the use of bootstrap Added the navbar js begin Extern of angular ui Extern of angular ui begins fixing the dialog when compiled Error messages do not appear when the dialog is submitted

---
+++ src/js_externs/ui-bootstrap-tpls-0.10.0.js
@@ -0,0 +1,11 @@
+/**
+ * @fileoverview Externs for Angular UI Bootstrap.
+ *
+ * @externs
+ */
+
+var angular = {};
+
+angular.$modalInstance = {};
+
+angular.$modalInstance.dismiss = function(reason){};
