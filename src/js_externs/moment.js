4f10e6995bc2ce3b981ec311f490cb5db7d9bda5
Paul Monteiro <paul.monteiro@codeshelf.com>
2014-09-06 16:33:32 -0700

    Added extern and moment library to the codeshelf.html to support compiled mode

---
+++ src/js_externs/moment.js
@@ -0,0 +1,40 @@
+//fake a type
+/**
+ * @constructor
+ **/
+var Moment = function(time) {
+
+};
+
+/**
+ * @param {number} num
+ * @param {string} units
+ * @return {Moment}
+ */
+Moment.prototype.add = function(num, units) {
+
+	};
+
+/**
+ * @param {Moment|number} time
+ * @return {Moment}
+ */
+Moment.prototype.isAfter = function(time) {
+
+	};
+
+/**
+ * @param  {string} format
+ * @return {string}
+ */
+Moment.prototype.format = function(format) {
+
+	};
+
+
+
+/**
+ * @param {?(Moment|number)} time
+ * @return {Moment}
+ */
+function moment(time) {}
