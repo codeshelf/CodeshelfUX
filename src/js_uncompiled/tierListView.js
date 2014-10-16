f0d5169f32ede95c40e53211600e416bf3096684
Paul Monteiro <paul.monteiro@codeshelf.com>
2014-10-16 10:42:46 -0700

    Correction "actions" quoting for compiled mode

--- src/js_uncompiled/tierListView.js
+++ src/js_uncompiled/tierListView.js
@@ -181,7 +181,7 @@ codeshelf.tierlistview = function(websession, facility, aisle) {
 	}];
 
 	var hierarchyMap = [];
-	hierarchyMap[0] = { "className": domainobjects['Tier']['className'], "linkProperty": 'parent', "filter" : tierFilter, "filterParams" : tierFilterParams, "properties": domainobjects['Tier']['properties'], "contextMenuDefs" : contextDefs, actions: actions };
+	hierarchyMap[0] = { "className": domainobjects['Tier']['className'], "linkProperty": 'parent', "filter" : tierFilter, "filterParams" : tierFilterParams, "properties": domainobjects['Tier']['properties'], "contextMenuDefs" : contextDefs, "actions": actions };
 
 	var viewOptions = {
 		'editable':  true,
