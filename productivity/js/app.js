6e950dfc3780fd8195b436b5e9d23210a6f6cd5a
Paul Monteiro <paul.monteiro@codeshelf.com>
2014-12-17 13:35:05 -0800

    Hard coded polling of productivity with a known facilityid

--- productivity/js/app.js
+++ productivity/js/app.js
@@ -1,20 +1,46 @@
 var React = require('react');
 var OrderDetailIBox = require('orderdetailibox');
+var $ = require('jquery');
 
-var el = document.getElementById('orderdetail');
 
-function summaryData(numComplete, numReleased, numInProgress, numShorts) {
-	return {
-		"complete" : numComplete,
-		"released" : numReleased,
-		"inprogress" : numInProgress,
-		"short" : numShorts
-	};
+
+
+function productivity() {
+	//https://localhost:8089/productivity/summary?facilityId=081a2d7a-7e12-4560-8351-dd4d07ccb4de;
+	var el = document.getElementById('orderdetail');
+	var serverEntryPoint = "https://localhost:8089";
+	var productivityPath = "/productivity/summary";
+	var facilityId = "081a2d7a-7e12-4560-8351-dd4d07ccb4de";
+	$.ajax(serverEntryPoint + productivityPath, {
+		data: {
+			"facilityId": facilityId
+		}
+	}).done(function(data) {
+		console.log("received productivity data", data);
+		var groups = data["groups"];
+		//only the first for now
+		for(var groupName in groups) {
+			var orderDetailSummaryData = groups[groupName];
+
+			//add created to released
+			var combined = orderDetailSummaryData["created"] + orderDetailSummaryData["released"];
+			orderDetailSummaryData["released"] = combined;
+			var props = {
+				"groupName": "12/15/2014",
+				orderDetailSummaryData: orderDetailSummaryData,
+				pickRate: 999
+			};
+
+			React.render(React.createElement(OrderDetailIBox, props), el);
+			break;
+		}
+
+	});
+
 }
 
-var props = {
-	"groupName": "12/15/2014",
-	orderDetailSummaryData: summaryData(10, 20, 5, 2),
-	pickRate: 999
+var poll = function() {
+	productivity();
+	setTimeout(poll, 3000);
 };
-React.render(React.createElement(OrderDetailIBox, props), el);
+poll();
