569c210e8dee2374f9455afec5a114640b95b2be
Paul Monteiro <paul.monteiro@codeshelf.com>
2014-12-22 13:55:22 -0800

    Clean up and documentation

--- productivity/js/app.js
+++ productivity/js/app.js
@@ -8,45 +8,39 @@ var NavHeader = require('components/nav').NavHeader;
 
 var csapi = require('data/csapi');
 
-
-
-
 var endpoint = "http://localhost:8088";
 csapi.getFacilities(endpoint).then(function(facilities) {
-			selectedFaclity(endpoint, facilities[0]);
+	//Hack to select first found facility
+	selectedFaclity(endpoint, facilities[0]);
 });
 
-
 function selectedFaclity(endpoint, facility) {
-	var props = {facility: facility
-				 };
-	React.render(React.createElement(NavHeader, props), $("li.nav-header").get(0));
+	//Render Navigation
+	React.render(React.createElement(NavHeader, {facility: facility }), $("li.nav-header").get(0));
 
 	var facilityId = facility.persistentId;
 
+	//Render Che Runs
 	csapi.getCheRuns(endpoint, facilityId).then(function(runs) {
 		console.log("The che runs", runs);
 	});
 
-
+	//Render Productivity for facility
 	csapi.getProductivity(endpoint, facilityId)
 		.then(function(productivityUpdate) {
+
 			var orderDetailComponents = toOrderDetailComponents(productivityUpdate);
+
+
 			var div = React.createElement("div", {className: "row orderdetails"}, orderDetailComponents);
-			var el = $('.wrapper-content').get(0);
-			React.render(div, el);
+			React.render(div, $('.wrapper-content').get(0));
 
 		});
-
-}
-
-function updateNavigationContext(facility) {
-
 }
 
 function toOrderDetailComponents(productivityUpdate) {
 	var groups = productivityUpdate["groups"];
-	//only the first for now
+	//Render an order detail component fro each group
 	var orderDetailComponents = [];
 	for(var groupName in groups) {
 		var orderDetailSummaryData = groups[groupName];
@@ -55,6 +49,8 @@ function toOrderDetailComponents(productivityUpdate) {
 		var combined = orderDetailSummaryData["created"] + orderDetailSummaryData["released"];
 		orderDetailSummaryData["released"] = combined;
 
+
+		//Render Order Detail for order group
 		var props = {
 			"groupName": groupName,
 			orderDetailSummaryData: orderDetailSummaryData,
@@ -68,8 +64,3 @@ function toOrderDetailComponents(productivityUpdate) {
 	}
 	return orderDetailComponents;
 }
-
-var poll = function() {
-	window.setTimeout(poll, 3000);
-};
-//poll();
