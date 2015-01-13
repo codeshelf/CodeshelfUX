b73d61431e4dfbdb75681df14362111ac7713810
Paul Monteiro <paul.monteiro@codeshelf.com>
2015-01-13 10:36:12 -0800

    Removed unused directories and files

--- test/GreeterTest.js
+++
@@ -1,8 +0,0 @@
-GreeterTest = TestCase("GreeterTest");
-
-GreeterTest.prototype.testGreet = function () {
-	var greeter = new myapp.Greeter();
-	assertEquals("Hello World!", greeter.greet("World"));
-	jstestdriver.console.log("JsTestDriver", greeter.greet("World"));
-	console.log(greeter.greet("Browser", "World"));
-};
