3299b00afe13d76ff4487f56ea3eb1d888e1333d
Paul Monteiro <paul.monteiro@codeshelf.com>
2015-01-19 09:51:38 -0800

    Fixed up bash syntax to be more portable with Mac

--- productivity/update-dependencies.sh
+++ productivity/update-dependencies.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
-NPM_CMD=$(type -P  npm &>  /dev/null && echo "Found")
+NPM_CMD=$(type -P  npm &>  /dev/null && echo "NPM Found")
 YUM_CMD=$(type -P yum &> /dev/null && echo "Found")
 APT_GET_CMD=$(type -P apt-get &> /dev/null && echo "Found")
 BREW_CMD=$(type brew &> /dev/null && echo "Found")
 
 #make sure npm is installed
-
-if [[ -z $NPM_CMD ]]; then
-    if [[ ! -z $YUM_CMD ]]; then
+if [ -z "$NPM_CMD" ]; then
+    if [ ! -z "$YUM_CMD" ]; then
         sudo yum install npm
-    elif [[ ! -z $APT_GET_CMD ]]; then
+    elif [ ! -z "$APT_GET_CMD" ]; then
         sudo apt-get install npm
         sudo ln -s /usr/bin/nodejs /usr/bin/node
-    elif [[ ! -z $BREW_CMD ]]; then
+    elif [ ! -z "$BREW_CMD" ]; then
         brew install node
     else
         echo "error can't install package npm"
