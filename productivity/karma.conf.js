cbf89d1cbadc35d00ab56f60e31d9d920a5111a0
Paul Monteiro <paul.monteiro@codeshelf.com>
2015-01-20 23:08:45 -0800

    Add harmony support for jsx in unit tests

--- productivity/karma.conf.js
+++ productivity/karma.conf.js
@@ -82,7 +82,7 @@ module.exports = function(config) {
                 module: {
                         loaders: [
                                 // Pass *.jsx files through jsx-loader transform
-                                { test: /\.jsx$/, loader: 'jsx' }
+                                { test: /\.jsx$/, loader: 'jsx-loader?harmony' }
                         ]
                 },
                 plugins: [
