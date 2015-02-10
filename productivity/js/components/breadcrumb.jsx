92fc97b676fc37b6f4db0747efa49a495f1fedfd
Paul Monteiro <paul.monteiro@codeshelf.com>
2015-02-09 16:49:51 -0800

    DEV-616 Refactor breadcrumb in productivity to be a bit more idiomatic

--- productivity/js/components/breadcrumb.jsx
+++ productivity/js/components/breadcrumb.jsx
@@ -1,36 +1,33 @@
 /** @jsx React.DOM */
 
 var React = require("react");
+var _ = require('lodash');
 var RClass = require("../helpers/react-helper");
+
 
 var Breadcrumb = RClass(function() {
     var crumbs = this.props.breadcrumbs;
 
     var renderedChildren = [];
-    var lastLabel = "";
-    for(var i=0; i < crumbs.length; i++) {
-	var crumb = crumbs[i];
-	var node = null;
-
-	if (i == crumbs.length-1) {
-	    node = (<li className="active" key={crumb.label}>
-                      <strong>{crumb.label}</strong>
-                    </li>);
-	}
-	else {
-	    node = (<li key={crumb.label}>
-                      <a href={crumb.href}>{crumb.label}</a>
-                    </li>);
-	}
-	lastLabel = crumb.label;
-     	renderedChildren.push(node);
-    }
+    var lastCrumb = _.last(crumbs);
+    var lastLabel = lastCrumb["label"];
     return (
             <div className="row wrapper border-bottom white-bg page-heading">
             <div className="col-lg-9">
             <div id="pageBreadcrumb">
             <h2>{lastLabel}</h2>
-            <ol className="breadcrumb">{ renderedChildren }</ol>
+            <ol className="breadcrumb">
+                {
+                   _.dropRight(crumbs, 1).map(function(crumb) {
+                       return (<li key={crumb.label}>
+                                  <a href={crumb.href}>{crumb.label}</a>
+                               </li>);
+                   })
+                }
+                <li className="active" key={lastCrumb.label}>
+                   <strong>{lastCrumb.label}</strong>
+                </li>
+            </ol>
             </div>
             </div>
             </div>);
