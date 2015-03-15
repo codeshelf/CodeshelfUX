567d59638dcb791d6b2c24abc5682a7b046079c6
Paul Monteiro <paul.monteiro@codeshelf.com>
2015-03-15 08:33:18 -0700

    Use tablesaw for data tables

--- productivity/js/components-display.jsx
+++ productivity/js/components-display.jsx
@@ -1,11 +1,14 @@
 var React = require('react');
 var _ = require('lodash');
 
-var Navbar = require('components/nav').Navbar;
+var Table = require('components/table').Table;
 
-var facilityNames = ['', 'F1', 'San Leandro', 'REALLY REALLY LONG FOR THE DESIGN'];
-_.forEach(facilityNames, function(facilityName){
-    var navHeaderNode = document.createElement('div');
-    document.body.appendChild(navHeaderNode);
-    React.render(<Navbar title={facilityName} navMenus={[]}/>, navHeaderNode);
-});
+var data = [
+                 "251935,251935.2,1,3/2/2015 12:00:00,3/2/2015 12:00:00,FEDEX,Arcu Eu Odio Corp.",
+
+                "251970,251970.3,1,3/2/2015 12:00:00,3/2/2015 12:00:00,UPS,Placerat Eget Inc.",
+                "251969,251969.2,1,3/2/2015 12:00:00,3/2/2015 12:00:00,ENVCO,Est Nunc Ullamcorper Corporation",
+                "251584,251584.23,15,3/2/2015 12:00:00,3/2/2015 12:00:00,LANDB,Libero Morbi Accumsan Company"
+        ];
+React.render(<Table rows={data}/>, document.body);
+
--- productivity/js/components/table.jsx
+++ productivity/js/components/table.jsx
@@ -1,6 +1,10 @@
 var React = require('react');
 var _ = require('lodash');
-var $ = require('jquery');
+//relies on jquery and tablesaw but just make sure it is included globally
+
+//var $ = require('jquery');
+//var tablesaw = require('tablesaw/dist/tablesaw');
+
 var Row = React.createClass({
     render: function() {
         var {data} = this.props;
@@ -11,7 +15,8 @@ var Row = React.createClass({
                                 {
 
                                     cells.map(function(value) {
-                                        return (<td key={i++}>{value}</td>);
+                                        var key=i++;
+                                        return (<td key={key}>{value}</td>);
                                     })
 
                                 }
@@ -32,7 +37,8 @@ var Header = React.createClass({
                 {
 
                     cells.map(function(value) {
-                        return (<th key={i++}>{value}</th>);
+                        var key = i++;
+                        return (<th key={key} scope="col" data-tablesaw-sortable-col data-tablesaw-priority={key == 0 ? "persist" : 0} >{value}</th>);
                     })
 
                 }
@@ -44,10 +50,15 @@ var Header = React.createClass({
 });
 
 var Table =  React.createClass({
+    componentDidMount: function() {
+        var node = this.getDOMNode();
+        var jqNode = $(node);
+        jqNode.table();
+    },
     render: function() {
         var {caption , rows} = this.props;
         return (
-                                <table className="table table-striped">
+                                <table className="tablesaw tablesaw-stack" data-tablesaw-mode="stack">
                 <caption>{caption}</caption>
                             <Header data="Order Id, Order Detail Id, Quantity, Order Date, Due Date, Shipper, Customer" />
                             <tbody>
