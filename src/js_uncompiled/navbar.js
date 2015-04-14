3462e1fe32bc0edd2fcdc003399563fe4d489f24
jonranstrom <jonranstrom@gmail.com>
2015-04-13 17:20:09 -0700

    DEV-713 need GTIN and Item masters to understand putwall

--- src/js_uncompiled/navbar.js
+++ src/js_uncompiled/navbar.js
@@ -68,30 +68,15 @@ codeshelf.Navbar.prototype.getNavbarItems = function(facility, authz, configValu
     if (typeof(configValues)=== 'undefined') configValues = [];
 	var navbarMenus = this.getMenuItems(this.navbar_, facility, configValues);
     var navbarItems = this.toFacilityNavbar(navbarMenus, facility);
-    navbarItems = this.toUserNavbar(navbarItems, authz);
+	navbarItems = this.toInventoryNavbar(navbarMenus, configValues);
+	navbarItems = this.toUserNavbar(navbarItems, authz);
     return navbarItems;
 };
 
 codeshelf.Navbar.prototype.getMenuItems = function(navbarMenus, facility, configValues) {
-	var showInvMenu = false;
-	for(var i=0; i<configValues.length; i++){
-
-		if (configValues[i].name == 'INVTMENU') {
-			if (configValues[i].value == "false") {
-				showInvMenu = false;
-			} else {
-				showInvMenu = true;
-			}
-			break;
-		}
-	}
-
+	// Now always have the inventory menu with ItemMaster and GTIN. The only decision is whether to have the Inventory Items.
+	// Might want more user level control of this later.
 	return this.filterNavbarMenus(navbarMenus, function(menu) {
-		if (menu['key'] == 'inventory') {
-			if (!showInvMenu) {
-				return false;
-			}
-		}
 		return true;
 
 	});
@@ -113,6 +98,36 @@ codeshelf.Navbar.prototype.toFacilityNavbar = function(navbarItems, facility) {
 
     });
 };
+
+codeshelf.Navbar.prototype.toInventoryNavbar = function(navbarItems, configValues) {
+	var showInvMenu = false;
+	for(var i=0; i<configValues.length; i++){
+
+		if (configValues[i].name == 'INVTMENU') {
+			if (configValues[i].value == "false") {
+				showInvMenu = false;
+			} else {
+				showInvMenu = true;
+			}
+			break;
+		}
+	}
+	return this.filterNavbar(navbarItems, function(item) {
+
+		if (item['key'] == 'inventory') {
+			if (!showInvMenu) {
+				return false;
+			}
+			else {
+				return true;
+			}
+		} else {
+			return true;
+		}
+
+	});
+};
+
 
 /**
  * @param {codeshelf.Authz} authz
