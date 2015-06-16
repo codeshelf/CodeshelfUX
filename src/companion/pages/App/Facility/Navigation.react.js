8018399ae045cbcb181c395b5c4f81b04aa3ff85
Paul Monteiro <paul.monteiro@codeshelf.com>
2015-06-16 16:31:41 -0700

    Enforce more permissions in navigation

--- src/companion/pages/App/Facility/Navigation.react.js
+++ src/companion/pages/App/Facility/Navigation.react.js
@@ -80,11 +80,11 @@ class Navigation extends React.Component {
                       <AuthzMenuItem to="overview" params={params} title="Overview" iconName="clock-o" />
                       <AuthzMenuItem to="orders" params={params} title="Orders" iconName="shopping-cart" />
                       <AuthzMenuItem permission="event:view"  to="blockedwork" params={params} title="Work Issues" iconName="exclamation-circle"/ >
-                      <AuthzMenuItem to="workresults" params={params} title="Work Results" iconName="pie-chart" />
+                      <AuthzMenuItem permission="event:view" to="workresults" params={params} title="Work Results" iconName="pie-chart" />
                       <AuthzMenuItem permission="worker:view" to="workermgmt" params={params} title="Manage Workers" iconName="users" />
                       <AuthzMenuItem  to="import" params={params} title="Manage Imports" iconName="upload" />
 
-                      <AuthzMenuItem  to="extensionpoints" params={params} title="Script Extensions" iconName="file-code-o" />
+                      <AuthzMenuItem permission="facility:edit" to="extensionpoints" params={params} title="Script Extensions" iconName="file-code-o" />
 
                       <AuthzMenuItem permission="ux:view" href={this.getUXUrl()} params={params} title="Configuration" iconName="cogs" />
                       <AuthzMenuItem permission="che:simulate" to="testscript" params={params} title="Test Scripts" iconName="bug" />
