278ea2ef2d8bd30ff60313c033def410c46e8552
Paul Monteiro <paul.monteiro@codeshelf.com>
2015-08-21 09:17:17 -0700

    Fix the call to delete orders

--- src/companion/pages/App/TestScript/TestScript.react.js
+++ src/companion/pages/App/TestScript/TestScript.react.js
@@ -103,7 +103,7 @@ export default class TestScript extends React.Component{
     }
 
     deleteOrders() {
-        return getFacilityContext().recreateFacility();
+        return getFacilityContext().deleteOrders();
     }
 
     recreateFacility() {
