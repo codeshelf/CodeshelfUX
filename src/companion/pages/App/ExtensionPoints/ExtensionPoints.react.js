416920a9f1dfc52f304579c40d5e19add66abe76
Paul Monteiro <paul.monteiro@codeshelf.com>
2015-09-08 12:32:32 -0700

    Make the title of the extension points consistent using import/export first

--- src/companion/pages/App/ExtensionPoints/ExtensionPoints.react.js
+++ src/companion/pages/App/ExtensionPoints/ExtensionPoints.react.js
@@ -10,10 +10,10 @@ import {fromJS, Map, List} from "immutable";
 import immstruct from "immstruct";
 
 const allTypes = fromJS([
-    {value: "OrderImportBeanTransformation",           label: "Order Import Record Change"},
-    {value: "OrderImportHeaderTransformation",         label: "Order Import Header Change"},
-    {value: "OrderImportCreateHeader",                 label: "Order Import Header Add"},
-    {value: "OrderImportLineTransformation",           label: "Order Import Line Change"},
+    {value: "OrderImportBeanTransformation",           label: "Import Order Record Change"},
+    {value: "OrderImportHeaderTransformation",         label: "Import Order Header Change"},
+    {value: "OrderImportCreateHeader",                 label: "Import Order Header Add"},
+    {value: "OrderImportLineTransformation",           label: "Import Order Line Change"},
     {value: "OrderOnCartContent",                      label: "Export OrderOnCart Change"},
     {value: "WorkInstructionExportContent",            label: "Export Work Inst. Content"},
     {value: "WorkInstructionExportCreateHeader",       label: "Export Work Inst. Header Add"},
