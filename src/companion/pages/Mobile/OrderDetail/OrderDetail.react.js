c7e4a76ded3c20fe258dd6d6d38e2ae4734621da
maty <mfedak@gmail.com>
2015-11-30 17:29:25 +0100

    created order search and order detail

--- src/companion/pages/Mobile/OrderDetail/OrderDetail.react.js
+++ src/companion/pages/Mobile/OrderDetail/OrderDetail.react.js
@@ -3,32 +3,65 @@ import exposeRouter from 'components/common/exposerouter';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 
-import {getOrderDetail} from './store';
-import {acSearch} from './store';
+import {Tabs, Tab} from 'react-bootstrap';
+
+import {getOrderDetail, TAB_DETAIL, TAB_ITEMS, TAB_PICKS} from './store';
+import {acSelectTab, acExpandItem} from './store';
+
+import {Items} from "./Items.react.js";
+import {Basics} from "./Basics.react.js";
+
 
 function mapDispatch(dispatch) {
-  return bindActionCreators({acSearch}, dispatch);
+  return bindActionCreators({acSelectTab, acExpandItem}, dispatch);
 }
 
 @connect(getOrderDetail, mapDispatch)
 class OrderDetail extends Component {
 
+  orderId = null;
 
   componentWillMount() {
     const {id: orderId} = this.props.router.getCurrentParams();
-    this.props.acSearch(orderId);
+    this.orderId = orderId;
+    this.props.acSelectTab(TAB_DETAIL, orderId, true);
+    console.log("After will mount");
+  }
+
+
+  renderTabs(activeTab) {
+    return (
+      <Tabs activeKey={activeTab} onSelect={(tab) => this.props.acSelectTab(tab, this.orderId)}>
+        <Tab eventKey={TAB_DETAIL} title="Info">Basic info about order</Tab>
+        <Tab eventKey={TAB_ITEMS} title="Items">Items for order</Tab>
+        <Tab eventKey={TAB_PICKS} title="Picks" disabled>Picks for order</Tab>
+      </Tabs>
+    );
   }
 
   render() {
+    console.log("OrderDetail render", this.props);
     const {id: orderId} = this.props.router.getCurrentParams();
-    const { whatIsLoading, error, order} = this.props;
-    if (whatIsLoading !== null || order === null) {
-      return <div> Loading ... </div>
+    const {tab} = this.props;
+    const {whatIsLoading, whatIsLoaded, error} = this.props[tab];
+    const showLoading = (whatIsLoading !== null || whatIsLoaded === null);
+    let contentElement = null;
+    if (showLoading) {
+      contentElement = <div> Loading ... </div>;
+    } else if (tab === TAB_DETAIL) {
+      contentElement = <Basics order={this.props[tab].order} />
+    } else if (tab === TAB_ITEMS) {
+      contentElement = <Items items={this.props[tab].items}
+                              expandedItem={this.props[tab].expandedItem}
+                              acExpandItem={this.props.acExpandItem}
+                              groupBy={this.props[tab].groupBy},
+                              sortingOrder={this.props[tab].sortingOrder} />
     }
     return (
       <div>
-        <h1>{order[0].orderId}</h1>
-        Order detail - {JSON.stringify(order)}
+        <h1>Order Id: {this.orderId}</h1>
+        {this.renderTabs(tab)}
+        {contentElement}
       </div>
     );
   }
--- src/companion/pages/Mobile/OrderDetail/store.js
+++ src/companion/pages/Mobile/OrderDetail/store.js
@@ -1,16 +1,53 @@
 import {getFacilityContext} from 'data/csapi';
 import {getSelectedFacility} from "../Facility/store";
 
-const LOADING_STARTED = "loading_of_order_detail_started";
-const LOADING_OK = "loading_of_order_detail_ok";
-const LOADING_ERROR = "loading_of_order_detail_error";
+const STATUS_STARTED = "started";
+const STATUS_OK = "ok";
+const STATUS_ERROR = "error";
+
+const SELECT_TAB = 'select tab for order detail';
+export const TAB_DETAIL = "tab detail";
+export const TAB_ITEMS = "tab items";
+export const TAB_PICKS = "tab picks";
+
+
+const LOADING_ = "loading of ";
+const LOADING_DETAIL = LOADING_ + TAB_DETAIL;
+const LOADING_ITEMS = LOADING_ + TAB_ITEMS;
+const LOADING_PICKS = LOADING_ + TAB_PICKS;
+
+const EXPAND_ITEM = "expand item";
+const GROUP_ITEMS = "group items";
+
+export const GROUP_ITEMS_WORKER = "group by workers";
+export const GROUP_ITEMS_TIMESTAMS = "group by timestamp";
+export const GROUP_ITEMS_STATUS = "group by status";
 
 
 const initState = {
-  whatIsLoading: null, // store what we are loading. Currently idOfItem
-                       // null value means nothing is loading
-  error: null,
-  order: null,
+  tab: TAB_DETAIL, // TAB_DETAIL, TAB_ITEMS, TAB_PICKS
+  [TAB_DETAIL]: {
+    whatIsLoaded: null, // store what we are loading. Currently idOfItem
+                        // null value means nothing is loading
+    order: null,
+    error: null,
+    whatIsLoading: null,
+  },
+  [TAB_ITEMS]: {
+    whatIsLoaded: null,
+    items: null,
+    error: null,
+    whatIsLoading: null,
+    expandedItem: null,
+    groupBy: GROUP_ITEMS_TIMESTAMS, // GROUP_ITEMS_WORKER, GROUP_ITEMS_TIMESTAMS, GROUP_ITEMS_STATUS
+    sortingOrder: 1, // can be 1 or -1
+  },
+  [TAB_PICKS]: {
+    whatIsLoaded: null,
+    picks: null,
+    error: null,
+    whatIsLoading: null,
+  },
 };
 
 export function getOrderDetail(state) {
@@ -19,68 +56,131 @@ export function getOrderDetail(state) {
 
 export function orderDetailReducer(state = initState, action) {
   switch (action.type) {
-    case LOADING_STARTED: {
-      console.log("!!!!!!! orderSearchReducer started !!!!");
-      const {whatIsLoading} = action;
-      let s = {...state, order: null, error: null, whatIsLoading: whatIsLoading}
-      console.log(s);
-      return s;
+    case LOADING_DETAIL: {
+      switch (action.status) {
+        case STATUS_STARTED: {
+          const {whatIsLoading} = action;
+          return {...state, [TAB_DETAIL]: {...(state[TAB_DETAIL]), order: null, error: null, whatIsLoading: whatIsLoading, whatIsLoaded: null}};
+        }
+        case STATUS_OK: {
+          const {data: order, orderId} = action;
+          return {...state, [TAB_DETAIL]: {...(state[TAB_DETAIL]), order, error: null, whatIsLoading: null, whatIsLoaded: orderId}};
+        }
+        case STATUS_ERROR: {
+          const {error} = action;
+          return {...state, [TAB_DETAIL]: {...(state[TAB_DETAIL]), order: null, error, whatIsLoading: null, whatIsLoaded: null}};
+        }
+      }
     }
-    case LOADING_OK: {
-      console.log("!!!!!!! LOADING_OK !!!!");
-      const {order} = action;
-      return {...state, order, error: null, whatIsLoading: null};
+    case LOADING_ITEMS: {
+      switch (action.status) {
+        case STATUS_STARTED: {
+          const {whatIsLoading} = action;
+          return {...state, [TAB_ITEMS]: {...(state[TAB_ITEMS]), items: null, error: null, whatIsLoading: whatIsLoading, whatIsLoaded: null}};
+        }
+        case STATUS_OK: {
+          const {data: items, orderId} = action;
+          return {...state, [TAB_ITEMS]: {...(state[TAB_ITEMS]), items, error: null, whatIsLoading: null, whatIsLoaded: orderId}};
+        }
+        case STATUS_ERROR: {
+          const {error} = action;
+          return {...state, [TAB_ITEMS]: {...(state[TAB_ITEMS]), items: null, error, whatIsLoading: null, whatIsLoaded: null}};
+        }
+      }
     }
-    case LOADING_ERROR: {
-      const {error} = action;
-      return {...state, order: null, error, whatIsLoading: null};
+    case EXPAND_ITEM: {
+      const {itemId: expandedItem} = action;
+      return {...state, [TAB_ITEMS]: {...(state[TAB_ITEMS]), expandedItem}};
+    }
+    case SELECT_TAB: {
+      const {tab} = action;
+      return {...state, tab};
     }
     default: return state;
   }
 }
 
-function searchStated(whatIsLoading) {
+export function acExpandItem(itemId) {
   return {
-    type: LOADING_STARTED,
-    whatIsLoading,
-  };
+    type: EXPAND_ITEM,
+    itemId
+  }
 }
 
-function searchOk(order) {
-  return {
-    type: LOADING_OK,
-    order,
-  };
-}
 
-function searchError(error) {
-  return {
-    type: LOADING_ERROR,
-    error,
-  };
-}
-
-export function acSearch(orderId) {
+export function acSelectTab(tab, orderId, forceLoad = false) {
   return (dispatch, getState) => {
-    dispatch(searchStated(orderId));
-    const selectedfacility = getSelectedFacility(getState());
-    if (!selectedfacility || !(selectedfacility.persistentId)) {
-      dispatch(searchError("Want to search for orders but no facility is provided"));
+    const prevTab = getOrderDetail(getState()).tab;
+    if (prevTab === tab) forceLoad = true;
+
+    if (tab != TAB_DETAIL && tab != TAB_ITEMS && tab != TAB_PICKS) {
+      throw "Unexpected tab setting: " + tab;
+    }
+    dispatch({
+      type: SELECT_TAB,
+      tab,
+      orderId,
+      forceLoad,
+    });
+    switch (tab) {
+      case TAB_DETAIL: return dispatch(acSearch(TAB_DETAIL, orderId, forceLoad));
+      case TAB_ITEMS: return dispatch(acSearch(TAB_ITEMS, orderId, forceLoad))
+    }
+  }
+}
+
+function search(tab, status, data) {
+  return {
+    type: LOADING_ + tab,
+    status,
+    ...data
+  };
+}
+
+
+// all avalible properties
+const orderDetailProperties = ["persistentId", "orderId", "customerId", "shipperId", "destinationId", "containerId",
+    "status", "orderLocationAliasIds", "groupUi", "active", "fullDomainId", "wallUi", "orderType", "dueDate", "orderDate"];
+
+
+// TODO optimize for speed
+function tabToApi(facilityId, tab) {
+  return {
+    [TAB_DETAIL]: (orderId) => getFacilityContext(facilityId).getOrder(orderDetailProperties, orderId),
+    [TAB_ITEMS]:  getFacilityContext(facilityId).getOrderDetails,
+    [TAB_PICKS]: getFacilityContext(facilityId).getOrder,
+  }[tab];
+}
+
+export function acSearch(tab, orderId, forceLoad) {
+  return (dispatch, getState) => {
+    const whatIsLoaded = getOrderDetail(getState())[tab].whatIsLoaded;
+    console.info(`search`, {tab, orderId, whatIsLoaded, forceLoad});
+    if ((whatIsLoaded === orderId) && (!forceLoad)) {
+      console.info(`Skip loading order info for ${orderId} beacuse is already loaded`);
       return;
     }
-    const getOrderDetails = getFacilityContext(selectedfacility.persistentId).getOrderDetails;
+    dispatch(search(tab, STATUS_STARTED, {whatIsLoading: orderId}));
 
-    getOrderDetails(orderId).then((data) => {
-      // Check if i should dispach or other search has been issued
-      const orderSearch = getOrderDetail(getState());
-      if (orderSearch.whatIsLoading === orderId) {
-        dispatch(searchOk(data));
-      }
-    }).catch((e) => {
-      console.log("Error from search orders", e);
+    const selectedfacility = getSelectedFacility(getState());
+    if (!selectedfacility || !(selectedfacility.persistentId)) {
+      dispatch(search(tab, STATUS_ERROR, {error: "Want to search for orders but no facility is provided"}));
+      return;
+    }
+    const getRequest = tabToApi(selectedfacility.persistentId, tab);
+
+    getRequest(orderId).catch((error) => {
+      console.error("Error from search orders", error);
       // Check if i should dispach
-      dispatch(searchError(e));
+      dispatch(search(tab, STATUS_ERROR, {error}));
+    })
+    .then((data) => {
+      console.log("data !!!!!!!!!!!", data);
+      const whatIsLoading = getOrderDetail(getState())[tab].whatIsLoading;
+      if (whatIsLoading === orderId) {
+        dispatch(search(tab, STATUS_OK, {data, orderId}));
+      }
     });
   }
-
 }
+
