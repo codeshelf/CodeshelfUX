d21d18b01d513d9cefbdc39a7178ab779b2a43e9
Paul Monteiro <paul.monteiro@codeshelf.com>
2015-12-21 11:49:42 -0800

    Change Home Page to List with Icons

--- src/companion/pages/Mobile/HomeSearch/HomeSearch.react.js
+++ src/companion/pages/Mobile/HomeSearch/HomeSearch.react.js
@@ -1,29 +1,44 @@
 import React, {Component} from 'react';
+import {Tabs, Tab, Row, Col, Button, ListGroup, ListGroupItem} from 'react-bootstrap';
 import { NavItemLink, MenuItemLink, ButtonLink} from '../links';
 import Icon from "react-fa";
 
 class HomeSearch extends Component {
   render() {
     return (
-      <div>
-        <ButtonLink bsStyle="link"
-          to="mobile-search-orders"
-          id="mobile-search-orders"
-          name="mobile-search-orders">
-            <Icon name="search" size="lg"/>
-            Orders
-        </ButtonLink>
-        {" "}
-        <ButtonLink bsStyle="link"
-          to="mobile-search-workers"
-          id="mobile-search-workers"
-          name="mobile-search-workers">
-            <Icon name="search" size="lg"/>
-            Workers
-        </ButtonLink>
-      </div>
+        <Row>
+          <Col>
+            <ListGroup>
+              <ListGroupItem>
+                <ButtonLink bsStyle="link"
+                    to="mobile-search-orders"
+                    id="mobile-search-orders"
+                    name="mobile-search-orders">
+                  <div className="thumbnail-wrapper d48 circular bg-primary text-white inline m-t-10">
+                    <Icon name="shopping-cart" size="lg"/>
+                  </div>
+                  <div className="p-l-10 inline p-t-5">
+                    <h3 className="m-b-5">Orders</h3>
+                  </div>
+                </ButtonLink>
+              </ListGroupItem>
+              <ListGroupItem>
+                <ButtonLink bsStyle="link"
+                    to="mobile-search-workers"
+                    id="mobile-search-workers"
+                    name="mobile-search-workers">
+                  <div className="thumbnail-wrapper d48 circular bg-primary text-white inline m-t-10">
+                    <Icon name="users" size="lg"/>
+                  </div>
+                  <div className="p-l-10 inline p-t-5">
+                    <h3 className="m-b-5">Workers</h3>
+                  </div>
+                </ButtonLink>
+              </ListGroupItem>
+            </ListGroup>
+          </Col>
+        </Row>
     );
   }
 }
-
 export default HomeSearch;
