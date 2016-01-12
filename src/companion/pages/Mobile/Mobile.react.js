4dedf3057bef2d5435ae7fbc45f70a094a9e776d
Andrej Skok <andrejskoki@gmail.com>
2016-01-12 16:12:36 +0100

    Handle error while loading data from local storage

--- src/companion/pages/Mobile/Mobile.react.js
+++ src/companion/pages/Mobile/Mobile.react.js
@@ -57,7 +57,13 @@ const createStoreWithMiddleware = compose(
 let store = createStoreWithMiddleware(rootReducer)
 
 // load data from local storage but only if version is greater than actual version
-const versionInStorage = localStorage.getItem(storageConfig.STORAGE_KEY_VERSION);
+let versionInStorage = -1;
+try {
+  versionInStorage = localStorage.getItem(storageConfig.STORAGE_KEY_VERSION);
+} catch (e) {
+  console.info("Error loading from storage. " + e.message);
+}
+
 if (versionInStorage >= storageConfig.VERSION) {
   storage.createLoader(storageEngine)(store);
 } else {
