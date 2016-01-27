1176ba880124fd16ff9a47b54c57b5b8f0b636a6
Paul Monteiro <paul.monteiro@codeshelf.com>
2016-01-27 15:29:58 -0800

    Correct click away of menu on iPhone

--- src/companion/Root.react.js
+++ src/companion/Root.react.js
@@ -19,11 +19,9 @@ class Root extends React.Component {
     componentDidMount() {
         document.addEventListener('keypress', this.onDocumentKeypress);
 
-        // Must be required here because there is no DOM in Node.js. Remember,
-        // mocking DOM in Node.js is an anti-pattern, because it can confuse
-        // isomorphic libraries. TODO: Wait for iOS fix, then remove.
+        // Return to codebase if we see problems on older devices
         // http://developer.telerik.com/featured/300-ms-click-delay-ios-8/
-        require('fastclick').attach(document.body);
+        //require('fastclick').attach(document.body);
 
         state.on('change', (newState, path) => {
             /*eslint-disable no-console */
