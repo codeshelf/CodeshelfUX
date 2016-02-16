5f7bd2c3308dda20d6f4d922082b0f3ce278a506
Paul Monteiro <paul.monteiro@codeshelf.com>
2016-02-16 13:24:20 -0800

    Changed menu for desktop

--- src/companion/pages/App/Facility/FacilityNavigation.react.js
+++ src/companion/pages/App/Facility/FacilityNavigation.react.js
@@ -22,17 +22,20 @@ class FacilityNavigation extends React.Component {
         <AuthzMenuItem permission="ux:view" to={`${basepath}/cartsearch`} params={params} title="Cart" iconName="search" />
         <AuthzMenuItem permission="ux:view" to={`${basepath}/workersearch`} params={params} title="Worker" iconName="search" />
         <AuthzMenuItem permission="event:view"  to={`${basepath}/blockedwork`} params={params} title="Issues" iconName="exclamation-circle"/ >
-        <Submenu title="Planning" iconName="tasks">
-          <AuthzMenuItem permission="order:view" to={`${basepath}/orders`} params={params} title="Orders" iconName="shopping-cart" />
+        <AuthzMenuItem permission="worker:edit" to={`${basepath}/workers`} params={params} title="Workers" iconName="users" />
+        <AuthzMenuItem permission="order:view" to={`${basepath}/orders`} params={params} title="Planning" iconName="tasks" />
+        <Submenu title="History" iconName="cogs">
           <AuthzMenuItem permission="workinstruction:view" to={`${basepath}/workinstructions`} params={params} title="Work Instructions" iconName="barcode" />
-          <AuthzMenuItem permission="worker:edit" to={`${basepath}/workers`} params={params} title="Workers" iconName="users" />
+          <AuthzMenuItem permission="order:view" to={`${basepath}/import`} params={params} title="Imports/Exports" iconName="exchange" />
         </Submenu>
-        {/* TODO: support *:import permissions */}
-        <AuthzMenuItem permission="order:view" to={`${basepath}/import`} params={params} title="Imports/Exports" iconName="exchange" />
-        <AuthzMenuItem permission="facility:edit" to={`${basepath}/extensionpoints`} params={params} title="Extensions" iconName="code" />
-        <AuthzMenuItem permission="facility:edit" to={`${basepath}/maintenance`} params={params} title="Maintenance" iconName="heartbeat" />
-        <AuthzMenuItem permission="ux:view" href={this.getUXUrl(facility)} params={params} title="Configuration" iconName="cogs" />
+        <Submenu title="Admin Facility" iconName="cogs">
+          {/* TODO: support *:import permissions */}
+          <AuthzMenuItem permission="facility:edit" to={`${basepath}/extensionpoints`} params={params} title="Extensions" iconName="code" />
+          <AuthzMenuItem permission="facility:edit" to={`${basepath}/maintenance`} params={params} title="Maintenance" iconName="heartbeat" />
+          <AuthzMenuItem permission="ux:view" href={this.getUXUrl(facility)} params={params} title="Configuration" iconName="cogs" />
+        </Submenu>
         <AuthzMenuItem permission="che:simulate" to={`${basepath}/testscript`} params={params} title="Test Scripts" iconName="bug" />
+
       </Navigation>);
   }
 
