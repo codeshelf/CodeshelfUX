aaf0411dfee63c41e5b3b667d77c7d1509700ef7
Andrej Skok <andrejskoki@gmail.com>
2016-02-29 17:01:20 +0100

    Links sketch

--- src/companion/pages/links.js
+++ src/companion/pages/links.js
@@ -6,7 +6,7 @@ import {connect} from 'react-redux';
 
 function mapState(state) {
   return {
-    facility: state.facility.selectedFacility
+    facility: state.facility && state.facility.selectedFacility,
   }
 }
 
@@ -17,10 +17,20 @@ function wrapFacility(Component) {
       if (this.props.params) {
         console.warn("old style Link detected");
       }
+      // && shouldHaveFacility
       if (!this.props.facility) {
         // no links will be renderd without facility
         return null;
-      } else {
+      } 
+      // else if (!this.props.facility && !shouldHaveFacility) {
+      //   let {to, ...rest} = this.props;
+      //   const basePath= location.hash.indexOf('#/mobile') === 0 ? '/mobile' : '';
+      //   if (to.indexOf("/") != 0) {
+      //     to=`${basePath}/${this.props.to}`;
+      //   }
+      //   return (<Component {...rest} to={to} />);
+      // }
+      else {
         const {to, ...rest} = this.props;
         let basePath=`/facilities/${this.props.facility.domainId}`;
         // little hacky for now, to be fixed when moving common thing one level higher
