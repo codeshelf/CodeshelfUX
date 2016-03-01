b61326bb6b83cd76b41a6fd21065b9b08aef6dc9
Richard Izip <richardizip@gmail.com>
2016-03-01 19:29:59 +0100

    demo: add and edit users
    * rough reworke to Redux with some unsolved errors
    * addUser should work
    * editUser throws error and reflects when page reloads
    
    * Errors
            * updateIn seems be undefined in EDIT_USER
            * active checkbox do not react on change

--- src/companion/pages/Users/UserForm.js
+++ src/companion/pages/Users/UserForm.js
@@ -1,10 +1,46 @@
-import  React from "react";
+import React from "react";
 import DocumentTitle from "react-document-title";
 import ModalForm from "components/common/ModalForm";
 import FormFields from "components/common/FormFields";
 import {Map} from "immutable";
 import {getEmail} from "data/user/store";
 import _ from "lodash";
+
+
+export function getFormMetadata() {
+    const commonRoles = [{value: "Admin", label: "Admin"},
+                 {value: "Upload", label: "Upload"},
+                 {value: "View", label: "View"},
+                 {value: "Supervise", label: "Supervise"},
+                 {value: "Dashboard", label: "Dashboard"},
+    ];
+    const csRoles = [{value: "CsDeveloper", label: "CsDeveloper"},
+                     {value: "CsSupport", label: "CsSupport"}];
+    let roles = commonRoles;
+
+    if (getEmail() && getEmail().indexOf("@codeshelf.com") > 0) {
+      roles = commonRoles.concat(csRoles);
+    }
+
+    return [
+      {name: "id",
+       label: "ID",
+       hidden: true},
+      {name: "username",
+       label: "Email",
+       required: true},
+      {name: "active",
+       label: "Active",
+       type: Boolean,
+       required: true},
+      {name: "roles",
+       label: "Roles",
+       options:roles,
+       type: Array,
+       required: false}
+    ];
+}
+
 
 export class UserForm extends React.Component{
 
@@ -13,66 +49,50 @@ export class UserForm extends React.Component{
         this.handleChange = this.handleChange.bind(this);
     }
 
-    handleChange(field, value) {
-        this.props.acUpdateAddUserForm(field.name, value);
-    }
-
     handleSave() {
-        const addUserForm = this.props.formData;
-        let promise = this.props.acAddUser(addUserForm);
+      if (this.userId) {
+        const userForm = this.props.formData;
+        let promise = this.props.acEditUser(userForm, this.userId);
         return promise;
-    }
-
-    getFormMetadata() {
-      const commonRoles = [{value: "Admin", label: "Admin"},
-                   {value: "Upload", label: "Upload"},
-                   {value: "View", label: "View"},
-                   {value: "Supervise", label: "Supervise"},
-                   {value: "Dashboard", label: "Dashboard"},
-      ];
-      const csRoles = [{value: "CsDeveloper", label: "CsDeveloper"},
-                       {value: "CsSupport", label: "CsSupport"}];
-      let roles = commonRoles;
-
-      if (getEmail() && getEmail().indexOf("@codeshelf.com") > 0) {
-        roles = commonRoles.concat(csRoles);
+      } else {
+        const userForm = this.props.formData;
+        let promise = this.props.acAddUser(userForm);
+        return promise;
       }
-
-      return [
-        {name: "id",
-         label: "ID",
-         hidden: true},
-        {name: "username",
-         label: "Email",
-         required: true},
-        {name: "active",
-         label: "Active",
-         type: Boolean,
-         required: true},
-        {name: "roles",
-         label: "Roles",
-         options:roles,
-         type: Array,
-         required: false}
-      ];
+      
+      // this was previoulsy in User.Edit, not sure what it does
+      /*let subFields = _.difference(fields, ["id", "username"]);
+      let params = _.pick(userForm, subFields);
+      params.roles = (params.roles.join) ? params.roles.join(",") : '';*/
     }
 
-    addFormMetadata() {
-      const fields = ["username", "roles"];
-      return _.filter(this.getFormMetadata(), (m) => fields.indexOf(m.name) >= 0);
+    handleChange(field, value) {
+        this.props.updateForm(field.name, value);
     }
 
     render() {
-        const {formData} = this.props;
-        const title = "New User";
         const returnRoute = "/admin/users";
+        const {formMetadata} = this.props;
+        let {formData} = this.props;
+
+        var user = undefined;
+        if (Object.keys(this.props.params).length) {
+          let {userId} = this.props.params;
+          let {users} = this.props;
+          user = new Map(users.find((u) => {
+            return u.id == parseInt(userId);
+          }));
+          this.userId = userId;
+          formData = user;
+        }
+        const title = (user !== undefined ? `Edit ${user.get('username')}` : "New user");
 
         return (<DocumentTitle title={title}>
                 <ModalForm title={title} returnRoute={returnRoute}
                  onSave={() => this.handleSave()}
                  formData={formData}>
 
-                <FormFields formMetadata={this.addFormMetadata()}
+                <FormFields formMetadata={formMetadata()}
                  handleChange={this.handleChange}
                  formData={formData}/>
                 </ModalForm>
