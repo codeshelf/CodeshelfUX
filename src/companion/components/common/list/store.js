535418f8728140270d052ef073ae5ed8f702bd6f
Andrej Skok <andrejskoki@gmail.com>
2016-03-08 14:02:59 +0100

    Fixes in list state

--- src/companion/components/common/list/store.js
+++ src/companion/components/common/list/store.js
@@ -18,32 +18,34 @@ const initState = new (Record({
 export function listReducer(state = initState, action) {
   switch (action.type) {
     case MOVE_COLUMNS: {
-      const columns = state.table.get('columns').slice(0);
+      const columns = state.tables.getIn([action.key, 'columns']).slice(0);
       const formerPosition = columns.indexOf(action.moved);
       const newPosition = columns.indexOf(action.afterName);
       columns.splice(formerPosition, 1)
       columns.splice(newPosition, 0, action.moved);
-      return state.setIn([action.key, 'columns'], columns);
+      return state.setIn(['tables', action.key, 'columns'], columns);
     }
     case SORT_COLUMN: {
-        return state.mergeIn([action.key, 'sortSpecs'], {[action.columnName]: {order: action.direction}});
+        return state.mergeIn(['tables', action.key, 'sortSpecs'], {[action.columnName]: {order: action.direction}});
     }
     default: return state;
   }
 }
 
-export function acMoveColumns(moved, afterName) {
+export function acMoveColumns(moved, afterName, key) {
   return {
     type: MOVE_COLUMNS,
     moved,
     afterName,
+    key,
   }
 }
 
-export function acSortColumn(columnName, direction) {
+export function acSortColumn(columnName, direction, key) {
   return {
     type: SORT_COLUMN,
     columnName,
     direction,
+    key,
   }
 }
