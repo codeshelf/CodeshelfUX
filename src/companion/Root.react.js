f93f28fdaff1562a8b1e1d00ad6c591f174369d0
Paul Monteiro <paul.monteiro@codeshelf.com>
2016-03-21 13:54:27 -0700

    Move old cursor down to Facility that wraps most old desktop components

--- src/companion/Root.react.js
+++ src/companion/Root.react.js
@@ -1,7 +1,5 @@
 /*eslint strict:0 */
 import React from 'react';
-import {state} from './data/state';
-import storage from 'lib/storage';
 import { DragDropContext } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
 import 'bootstrap/less/bootstrap.less';
@@ -26,55 +24,10 @@ class Root extends React.Component {
         }
         return false;
       };
-
-        document.addEventListener('keypress', this.onDocumentKeypress);
-
-        // Return to codebase if we see problems on older devices
-        // http://developer.telerik.com/featured/300-ms-click-delay-ios-8/
-        //require('fastclick').attach(document.body);
-
-        state.on('change', (newState, path) => {
-            /*eslint-disable no-console */
-            console.time('whole app rerender');
-            this.forceUpdate(() => {
-                console.timeEnd('whole app rerender');
-            });
-
-            if (path != null && path.indexOf("preferences") >= 0) {
-                storage.set("preferences", state.cursor(["preferences"])());
-            }
-        /*eslint-enable */
-       });
-   }
-
-   componentWillUnmount() {
-       document.removeEventListener('keypress', this.onDocumentKeypress);
-   }
-
-   onDocumentKeypress(e) {
-       // Press shift+ctrl+s to save app state and shift+ctrl+l to load.
-       if (!e.shiftKey || !e.ctrlKey) return;
-       switch (e.keyCode) {
-       case 19:
-           window._appState = state.save();
-           window._appStateString = JSON.stringify(window._appState);
-           /*eslint-disable no-console */
-           console.log('app state saved');
-           console.log('copy the state to your clipboard by calling copy(_appStateString)');
-           console.log('for dev type _appState and press enter');
-           /*eslint-enable */
-           break;
-       case 12:
-           const stateStr = window.prompt('Path the serialized state into the input'); // eslint-disable-line no-alert
-           const newState = JSON.parse(stateStr);
-           if (!newState) return;
-           state.load(newState);
-           break;
-       }
    }
 
    render() {
-     return React.cloneElement(this.props.children, { state: state });
+     return (this.props.children);
 
    }
 };
--- src/companion/pages/App/Facility/Facility.react.js
+++ src/companion/pages/App/Facility/Facility.react.js
@@ -7,10 +7,62 @@ import TopNavBar from '../TopNavBar.react';
 import Footer from '../Footer.react.js';
 
 //temp old store
-import {getSelectedTenant} from 'data/user/store';
 import {state} from 'data/state';
+import storage from 'lib/storage';
+import {getSelectedTenant} from 'data/user/store';
 
 export default class Facility extends React.Component {
+
+    componentDidMount() {
+      document.addEventListener('keypress', this.onDocumentKeypress);
+
+      // Return to codebase if we see problems on older devices
+      // http://developer.telerik.com/featured/300-ms-click-delay-ios-8/
+      //require('fastclick').attach(document.body);
+
+      state.on('change', (newState, path) => {
+        /*eslint-disable no-console */
+        console.time('whole app rerender');
+        this.forceUpdate(() => {
+          console.timeEnd('whole app rerender');
+        });
+
+        if (path != null && path.indexOf("preferences") >= 0) {
+          storage.set("preferences", state.cursor(["preferences"])());
+        }
+  /*eslint-enable */
+      });
+
+
+    }
+
+    componentWillUnmount() {
+      state.removeAllListeners();
+      document.removeEventListener('keypress', this.onDocumentKeypress);
+    }
+
+    onDocumentKeypress(e) {
+      // Press shift+ctrl+s to save app state and shift+ctrl+l to load.
+      if (!e.shiftKey || !e.ctrlKey) return;
+        switch (e.keyCode) {
+          case 19:
+              window._appState = state.save();
+              window._appStateString = JSON.stringify(window._appState);
+              /*eslint-disable no-console */
+              console.log('app state saved');
+              console.log('copy the state to your clipboard by calling copy(_appStateString)');
+              console.log('for dev type _appState and press enter');
+              /*eslint-enable */
+              break;
+          case 12:
+              const stateStr = window.prompt('Path the serialized state into the input'); // eslint-disable-line no-alert
+              const newState = JSON.parse(stateStr);
+              if (!newState) return;
+              state.load(newState);
+          break;
+        }
+    }
+
 
     render() {
         let {selected, availableFacilities, acToggleSidebar} = this.props;
